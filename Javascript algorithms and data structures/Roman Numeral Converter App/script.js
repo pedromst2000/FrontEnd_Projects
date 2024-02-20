addcf1387984e4f63e9290a3e4aee5ea27176416
pedromst2000 <40210465@esmad.ipp.pt>
2024-02-20 17:23:28 +0000

    refactor: Add missing Jsdocs to convertRoman fun

--- Javascript algorithms and data structures/Roman Numeral Converter App/script.js
+++ Javascript algorithms and data structures/Roman Numeral Converter App/script.js
@@ -3,6 +3,9 @@
  * @description - This function takes a number and converts it into a roman numeral.
  * @param {number} num - The number to convert.
  * @returns {string} - The converted number.
+ * @throws {Error}  - Please enter a valid number.
+ * @throws {Error} - Please enter a number greater than or equal to 1.
+ * @throws {Error} - Please enter a number less than or equal to 3999.
  * @example
  * convertToRoman(2) // "II"
  * convertToRoman(3) // "III"
@@ -13,6 +16,24 @@
  */
 
 const convertToRoman = (num) => {
+  /**
+   * @constant {object} romanNumeral - An object that contains the roman numeral and its equivalent number.
+   * @description - This object contains the roman numeral and its equivalent number.
+   * @property {number} M - 1000
+   * @property {number} CM - 900
+   * @property {number} D - 500
+   * @property {number} CD - 400
+   * @property {number} C - 100
+   * @property {number} XC - 90
+   * @property {number} L - 50
+   * @property {number} XL - 40
+   * @property {number} X - 10
+   * @property {number} IX - 9
+   * @property {number} V - 5
+   * @property {number} IV - 4
+   * @property {number} I - 1
+   */
+
   const romanNumeral = {
     M: 1000,
     CM: 900,
@@ -55,7 +76,6 @@ const convertToRoman = (num) => {
   }
 };
 
-
 console.log(convertToRoman(2)); // "II"
 console.log(convertToRoman(3)); // "III"
 console.log(convertToRoman(435)); // "CDXXXV"
@@ -63,47 +83,46 @@ console.log(convertToRoman(4300)); // "Please enter a number less than or equal 
 console.log(convertToRoman(-1)); // "Please enter a number greater than or equal to 1"
 console.log(convertToRoman("hello")); // "Please enter a valid number"
 
+document.getElementById("roman-form").onsubmit = (e) => {
+  e.preventDefault();
+  const num = document.getElementById("number").value;
+  const output = document.getElementById("output");
 
-// document.getElementById("roman-form").onsubmit = (e) => {
-//   e.preventDefault();
-//   const num = document.getElementById("number").value;
-//   const output = document.getElementById("output");
+  // get the error message
+  const errorMessage = convertToRoman(num);
 
-//   // get the error message
-//   const errorMessage = convertToRoman(num);
+  if (
+    errorMessage === "Please enter a valid number" ||
+    errorMessage === "Please enter a number greater than or equal to 1" ||
+    errorMessage === "Please enter a number less than or equal to 3999"
+  ) {
+    output.innerHTML = errorMessage;
+    output.style.opacity = 1;
+    output.style.backgroundColor = "#ff4d4d";
+    output.animate([{ opacity: 0 }, { opacity: 1 }], {
+      // timing options
+      duration: 550,
+    });
 
-//   if (
-//     errorMessage === "Please enter a valid number" ||
-//     errorMessage === "Please enter a number greater than or equal to 1" ||
-//     errorMessage === "Please enter a number less than or equal to 3999"
-//   ) {
-//     output.innerHTML = errorMessage;
-//     output.style.opacity = 1;
-//     output.style.backgroundColor = "#ff4d4d";
-//     output.animate([{ opacity: 0 }, { opacity: 1 }], {
-//       // timing options
-//       duration: 550,
-//     });
+    setTimeout(() => {
+      output.style.opacity = 0;
+      output.animate([{ opacity: 1 }, { opacity: 0 }], {
+        duration: 550,
+      });
+    }, 2500);
+  } else {
+    output.innerHTML = convertToRoman(num);
+    output.style.opacity = 1;
+    output.style.backgroundColor = "rgb(113, 116, 144)";
+    output.animate([{ opacity: 0 }, { opacity: 1 }], {
+      duration: 550,
+    });
 
-//     setTimeout(() => {
-//       output.style.opacity = 0;
-//       output.animate([{ opacity: 1 }, { opacity: 0 }], {
-//         duration: 550,
-//       });
-//     }, 2500);
-//   } else {
-//     output.innerHTML = convertToRoman(num);
-//     output.style.opacity = 1;
-//     output.style.backgroundColor = "rgb(113, 116, 144)";
-//     output.animate([{ opacity: 0 }, { opacity: 1 }], {
-//       duration: 550,
-//     });
-
-//     setTimeout(() => {
-//       output.style.opacity = 0;
-//       output.animate([{ opacity: 1 }, { opacity: 0 }], {
-//         duration: 550,
-//       });
-//     }, 5000);
-//   }
-// };
+    setTimeout(() => {
+      output.style.opacity = 0;
+      output.animate([{ opacity: 1 }, { opacity: 0 }], {
+        duration: 550,
+      });
+    }, 5000);
+  }
+};
