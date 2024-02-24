9bf7f3497e62262b8106865c3855b19216cc4d61
pedromst2000 <40210465@esmad.ipp.pt>
2024-02-24 18:09:10 +0000

    feat: palindrome checker challenge

---
+++ Javascript algorithms and data structures/Palindrome Checker/script.js
@@ -0,0 +1,28 @@
+/**
+ * @function isPalindrome
+ * @description A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+ * @param {String} str
+ * @returns  {Boolean}
+ * @example
+ * isPalindrome("eye") // true
+ * isPalindrome("race car") // true
+ * isPalindrome("five|_/|four") // false
+ * isPalindrome("1 eye for 1 eye") // false
+ */
+
+const isPalindrome = (str) => {
+  const regex = /[_\W]/g;
+  str = str.replace(regex, "").toLowerCase();
+
+  let reverseString = str.split("").reverse().join("");
+
+  if (str == reverseString) return true;
+
+  return false;
+};
+
+console.log(isPalindrome("eye")); // true
+console.log(isPalindrome("race car")) // true
+console.log(isPalindrome("five|_/|four")) // false
+console.log(isPalindrome("1 eye for 1 eye")) // false
+
